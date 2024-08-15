96fb6d30704ed420579b8f60dcc3e6b5e5b5483a
Atul Y <99254634+ATUL9372@users.noreply.github.com>
2024-08-15 05:36:28 +0530

    Update K8M.sh

--- K8M.sh
+++ K8M.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
+echo "Install Docker and curl"
+
+sudo apt-get update -y
+sudo apt-get install docker.io -y
 sudo apt install curl -y
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO https://dl.k8s.io/release/v1.31.0/bin/linux/amd64/kubectl
-curl -LO https://dl.k8s.io/release/v1.31.0/bin/linux/arm64/kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 
-echo "---------- Install kubectl ----------"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
 
-echo "---------- Install kubectl version  ----------"
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+
+
+echo "----------------- Update/Install kubectl on ubuntu -----------------"
+sudo apt-get update -y
+sudo apt-get install -y kubectl
+
+echo "----------------- kubectl version-----------------"
+snap install kubectl --classic
 kubectl version --client
 
 
-echo "---------- Install kubectl version yaml formate ----------"
-kubectl version --client --output=yaml
+
+echo "----------------- Install minikube on ubuntu -----------------"
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 
 
-echo "----------------- Install Minikube on ubuntu -----------------"
+echo "----------------- Minikube status -----------------"
+
+minikube status
